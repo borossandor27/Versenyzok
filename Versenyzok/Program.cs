4b65f6134262cc58dcea27a983f11d1ebfa6271c
borossandor27 <boros.sanyi@gmail.com>
2021-02-09 22:40:55 +0100

    Add project files.

---
+++ Versenyzok/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Versenyzok
+{
+    class Program
+    {
+        static List<Versenyzo> Versenyzok = new List<Versenyzo>();
+        static void Main(string[] args)
+        {
+            Beolvas("pilotak.csv");
+            Console.WriteLine($"3. feladat: {Versenyzok.Count}");
+            Console.WriteLine($"\n4. feladat: {Versenyzok.Last().Nev}");
+            Console.WriteLine($"\n5. feladat:");
+            foreach (var item in Versenyzok.FindAll(a => a.Szuletesi_datum < DateTime.Parse("1901-01-01")))
+            {
+                Console.WriteLine($"\t{item.Nev} ({item.Szuletesi_datum.ToString("yyyy. MM. dd.")})");
+            }
+
+            int rajtszam = Versenyzok.FindAll(a => a.Rajtszam != 0).Min(b => b.Rajtszam);
+            //-- 6. feladat -------------------------------
+            Console.WriteLine($"\n6. feladat: {Versenyzok.Find(a => a.Rajtszam==rajtszam).Nemzetiseg}");
+            //-- 7. feladat -------------------------------
+             var ver = Versenyzok.FindAll(a => a.Rajtszam > 0).GroupBy(b => b.Rajtszam).Select(c => new { rsz = c.Key, db = c.Count() }).Where(d => d.db > 1);
+
+            Console.WriteLine($"\n7. feladat: {string.Join(", ",ver.Select(a => a.rsz))}");
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+
+        static void Beolvas(string fajl)
+        {
+            using (StreamReader sr = new StreamReader(fajl))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    Versenyzok.Add(new Versenyzo(sr.ReadLine().Split(';')));
+                }
+            }
+        }
+    }
+}
---
+++ Versenyzok/Versenyzo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versenyzok
+{
+    class Versenyzo
+    {
+        //-- név;születési_dátum;nemzetiség;rajtszám
+        string nev;
+        DateTime szuletesi_datum;
+        string nemzetiseg;
+        int rajtszam;
+
+        public Versenyzo(string[] sor)
+        {
+            nev = sor[0].Trim();
+            szuletesi_datum = DateTime.Parse(sor[1].Trim());
+            nemzetiseg = sor[2];
+            int.TryParse(sor[3].Trim(), out rajtszam);
+            //rajtszam = int.Parse();
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public DateTime Szuletesi_datum { get => szuletesi_datum; set => szuletesi_datum = value; }
+        public string Nemzetiseg { get => nemzetiseg; set => nemzetiseg = value; }
+        public int Rajtszam { get => rajtszam; set => rajtszam = value; }
+    }
+}
